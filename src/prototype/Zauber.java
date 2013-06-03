e6693521f7398421212c0eb325378c137c7b620a
Christian Schilling <christian.schilling@uni-duesseldorf.de>
2013-06-03 17:17:21 +0200

    bugfix

--- src/prototype/Zauber.java
+++ src/prototype/Zauber.java
@@ -11,7 +11,7 @@ public class Zauber {
 	private BufferedImage bimg;
 	private float f_Zaubergeschwindigkeitx;
 	private float f_Zaubergeschwindigkeity;
-	private static Rectangle bounding;
+	private Rectangle bounding;
 	private List<Zauber> Zaubern;
 	private float f_playposx;
 	private float f_playposy;
